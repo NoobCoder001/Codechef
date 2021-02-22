87a86654842dd0f7efd539de825008a628fb9e44
NoobCoder001 <67992037+NoobCoder001@users.noreply.github.com>
2021-02-22 23:06:49 +0530

    Add files via upload

---
+++ Long Challenges/Feb 2021/Highest_Divisor.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+# define INF 0x3f3f3f3f
+#define all(c) c.begin(),c.end()
+#define deb(x) cout<<"[ "<<#x<<" = "<<x<<"] "
+#define mp(x,y) make_pair(x,y)
+#define pb push_back
+#define f first
+#define s second
+using namespace std;
+
+
+ll gcd(ll a, ll b)
+{
+    if (a == 0)
+    return b;
+    return gcd(b % a, a);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin>>n;
+ 
+    int ans = -1; 
+
+    for(int i=10;i>=1;i--)
+    {
+        if(n%i==0)
+        {
+            ans = i;
+            break;
+        }
+    } 
+
+    cout<<ans<<endl;
+
+
+
+    return 0;
+}
